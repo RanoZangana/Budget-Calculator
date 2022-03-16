4d37f6275ea54b4b8954883edc954f86c5f0a138
Rafiki <zangana.rano@gmail.com>
2022-03-16 15:35:11 +0100

    dasdad

--- budgetcalculator/src/Searchform.js
+++ budgetcalculator/src/Searchform.js
@@ -5,54 +5,57 @@ import CurrencyInput from 'react-currency-input-field';
 
 function SearchForm() {
 
-  const [currency, setCurrency] = useState();
-  const [category, setCategory] = useState();
-  const [date, setDate] = useState();
-  return (
-    <div className="SearchForm">
-      <form>
-        <CurrencyInput
-          id='price'
-          placeholder='Enter a certain amount'
-          required
-          decimalsLimit={2}
-          defaultValue={0}
-          value={currency}
-          onChange={(e) => setCurrency(e.target.value)}
-        />
-        <select
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="" disabled selected>Choose Category</option>
-          <option value="entertainment">Entertainment</option>
-          <option value="housing">Housing</option>
-          <option value="transportation">Transportation</option>
-          <option value="food">Food</option>
-          <option value="insurance">Insurance</option>
-          <option value="healthcare">Healthcare</option>
-          <option value="savings">Savings</option>
-          <option value="utilities">Utilities</option>
-          <option value="miscellanous">Miscellanous</option>
-        </select>
-        <input
-          id='date'
-          type="date"
-          required
-          min="1000-01-01"
-          max="9999-12-31"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <button onClick={submitInput} >button</button>
-      </form>
-      <Items />
-    </div>
-  );
-}
-const submitInput = event => {
-  event.preventDefault();
-  alert('hello');
+    const [currency, setCurrency] = useState();
+    const [category, setCategory] = useState();
+    const [date, setDate] = useState();
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        const report = {currency, category, date};
+        console.log(report);
+    }
+    return (
+        <div className="SearchForm">
+            <form onSubmit={handleSubmit}>
+                <CurrencyInput
+                    id='price'
+                    placeholder='Enter a certain amount'
+                    required
+                    decimalsScale={2}
+                    defaultValue={0}
+                    maxLength = {10}
+                    value={currency}
+                    onChange={(e) => setCurrency(e.target.value)}
+                />
+                <select
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                >
+                    <option value="" disabled selected>Choose Category</option>
+                    <option value="entertainment">Entertainment</option>
+                    <option value="housing">Housing</option>
+                    <option value="transportation">Transportation</option>
+                    <option value="food">Food</option>
+                    <option value="insurance">Insurance</option>
+                    <option value="healthcare">Healthcare</option>
+                    <option value="savings">Savings</option>
+                    <option value="utilities">Utilities</option>
+                    <option value="miscellanous">Miscellanous</option>
+                </select>
+                <input
+                    id='date'
+                    type="date"
+                    required
+                    min="1000-01-01"
+                    max="9999-12-31"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                />
+                <button>Add Report</button>
+            </form>
+            <Items />
+        </div>
+    );
 }
 
 export default SearchForm;
