016f66ec4b7beee542fe2fef1cfdf8ec3648bdbb
MichaelKimelman <91721421+MichaelKimelman@users.noreply.github.com>
2022-03-17 17:15:33 +0100

    Diverse saker

--- budgetcalculator/src/Searchform.js
+++ budgetcalculator/src/Searchform.js
@@ -5,24 +5,36 @@ import CurrencyInput from 'react-currency-input-field';
 
 function SearchForm() {
 
-    const [currency, setCurrency] = useState();
-    const [category, setCategory] = useState();
-    const [date, setDate] = useState();
+    const [currency, setCurrency] = useState("");
+    const [category, setCategory] = useState("");
+    const [date, setDate] = useState("");
+    const [items, setItems] = useState([]);
+
+    function addItem(currency, category, date){
+        const newItems = [...items, { currency, category, date}];
+        setItems(newItems);
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const report = { currency, category, date };
         console.log(report);
+        addItem( currency, category, date);
     }
     return (
         <div className="SearchForm">
             <form onSubmit={handleSubmit}>
-                <input type="number" onChange={(e) => setCurrency(e.target.value)} step="0.001"></input>
+                <input 
+                type="number" 
+                onChange={(e) => setCurrency(e.target.value)} 
+                step="any"
+                required
+                ></input>
                 <select
-                    value={category}
+                    required
                     onChange={(e) => setCategory(e.target.value)}
                 >
-                    <option value="" disabled selected>Choose Category</option>
+                    <option value="">Choose Category</option>
                     <option value="entertainment">Entertainment</option>
                     <option value="housing">Housing</option>
                     <option value="transportation">Transportation</option>
@@ -31,7 +43,7 @@ function SearchForm() {
                     <option value="healthcare">Healthcare</option>
                     <option value="savings">Savings</option>
                     <option value="utilities">Utilities</option>
-                    <option value="miscellanous">Miscellanous</option>
+                    <option value="miscellaneous">Miscellaneous</option>
                 </select>
                 <input
                     id='date'
