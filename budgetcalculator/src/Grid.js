8c815aa03dbfacd3f8d24b64abf31dd1f17dc4c3
JuliaLom <loman.julia@gmail.com>
2022-03-18 14:21:08 +0100

    css

--- budgetcalculator/src/Grid.js
+++ budgetcalculator/src/Grid.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import SearchForm from './Searchform';
 
 export default function Grid () {
   
     return (
 
         <div className="container">
-          <div className="item">1</div>
+          <input id='amount'></input>
           <div className="item">2</div>
           <div className="item">3</div>
           <div className="item">4</div>
